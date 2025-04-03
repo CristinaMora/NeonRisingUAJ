c44200bacd3843642ee2a39be39af62afd3d6833
LauraWangQiu <yiwang03@ucm.es>
2025-04-04 01:20:24 +0200

    creacion clase Tracker con estructura modificable y con comentarios + generador de IDs unicos

---
+++ Nombre del juego/Assets/scripts/Telemetry/Tracker.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static UnityEditor.PlayerSettings;
+
+// Al diseñarlo hay que tener en cuenta qué vamos a hacer cuando se producen ciertos
+// eventos catastróficos:
+// • Nos hemos quedado sin espacio.
+// • No tenemos red. -> guardar en local hasta que haya red
+// • No tenemos permisos para almacenar los eventos.
+// • Consumo de batería y datos (móviles).
+// • El usuario reinstala la aplicación (dejamos de tener info del usuario concreto, si
+// fuese necesario).
+// • El usuario modifica los datos enviados (poco probable en métricas).
+// • Privacidad en distintas regiones/países.
+
+public class Tracker
+{
+    public enum Format { JSON, CSV };
+    public enum PersistenceType { LOCAL, NETWORK };
+    private string DEFAULT_SERVER_DOMAIN = "https://example.com";
+    private const int EVENTS_TO_WRITE_SIZE = 50;
+    private const string SALT = "UAJ-Grupo1";
+    // TODO: Crear struct Event con su sessionId, id, timestamp
+    // TODO: Crear eventos genericos
+    // TODO: Manejar eventos puntuales donde se guardan en escenas especificas
+    // TODO: Manejar eventos muestreables (solamente escribir cada cierto tiempo)
+
+    private FileStream logFile;
+    // TODO: Crear la cola de eventos, importante investigar sobre la concurrencia
+    // mientras se está leyendo y eliminando de la cola, también se están añadiendo
+    // eventos...
+    //private ConcurrentQueue<Event> eventQueue;
+    Format format;
+
+    public Tracker(string logFilename, Format chosenFormat, PersistenceType persType)
+    {
+        switch (persType)
+        {
+            case PersistenceType.LOCAL:
+                CreateLocalLogFile(logFilename);
+                break;
+            case PersistenceType.NETWORK:
+                InitiateNetworkConnection(logFilename);
+                break;
+            default: break;
+        }
+
+        // ...
+    }
+
+    /*
+     * Genera un id unico a partir del evento
+     * Utiliza el tiempo actual, una salt
+     */
+    private string GenerateUniqueID(/*Event event*/)
+    {
+        //using (var sha256 = SHA256.Create())
+        //{
+        //    var epoch = new DateTime(1970, 1, 1);
+        //    var millisecondsSinceEpoch = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
+        //    var rawData = $"{event.sessionId}-{SALT}-{millisecondsSinceEpoch}";
+        //    var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+        //    return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        //}
+        return "";
+    }
+
+    /*
+     * Crear y abrir el archivo donde volcar
+     * los datos
+     */
+    private void CreateLocalLogFile(string logFilename)
+    {
+
+    }
+
+    // Cerrar el FileStream??
+
+    /*
+     * Posibilidad de enviar a un servidor los datos
+     */
+    private void InitiateNetworkConnection(string logFilename)
+    {
+        // Comprobar que se pueda conectar y enviar datos
+        // Si no se puede en esta primera vez, volver a intentarlo
+        // cada X tiempo y mientras tanto guardar los datos en local
+    }
+
+    // TODO: Singleton
+    // TODO: SendEvent()
+    // TODO: Write()
+
+    /*
+     * Se llama desde el resto de scripts:
+     * almacena el nuevo evento en la cola
+     */
+    public void SendEvent(/*Event event*/)
+    {
+        // Aplicar el sessionId, id, timestamp
+    }
+
+    /*
+     * Escribe el par de [clave,valor]
+     */
+    public void Write(string key/*, ... value*/)
+    {
+
+    }
+
+    // TODO: Bucle de lectura-escritura del archivo de guardado
+    // que dependiendo del PersistenceType se escribirá/enviará
+    // en local o por servidor
+}
