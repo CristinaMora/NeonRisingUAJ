91174dcd82a99cdb7ed692333f03849e0a8a7f8f
CiscoGalvan <fragalva@ucm.es>
2025-06-17 16:38:21 +0200

    Fallos que pueden producirse al abrirse el fichero

--- Nombre del juego/Assets/scripts/Telemetry/Persistance/FilePersistence.cs
+++ Nombre del juego/Assets/scripts/Telemetry/Persistance/FilePersistence.cs
@@ -1,11 +1,5 @@
-using Firebase;
-using Firebase.Database;
-using Firebase.Extensions;
 using System;
-using System.Collections.Concurrent;
 using System.IO;
-using System.Net.Http;
-using System.Net.Mail;
 using System.Text;
 using UnityEngine;
 using static Tracker;
@@ -25,47 +19,55 @@ public class FilePersistence : Persistence
     /// </summary>
     private void CreateLocalLogFile()
     {
-        localPath = Application.dataPath + "/" + ConfigManager.GetLogFilename();
-        // Para cada formato añadimos la extensión correspondiente.
-        switch (format)
+        try
         {
-            case Format.CSV:
-                localPath += ".csv";
-                break;
-            case Format.JSON:
-                localPath += ".json";
+			localPath = Application.dataPath + "/" + ConfigManager.GetLogFilename();
+			// Para cada formato añadimos la extensión correspondiente.
+			switch (format)
+			{
+				case Format.CSV:
+					localPath += ".csv";
+					break;
+				case Format.JSON:
+					localPath += ".json";
 
-                // Si no existe el archivo, lo creamos y escribimos el inicio del JSON
-                if (!File.Exists(localPath))
-                {
-                    // No existe: lo creamos y escribimos [
-                    File.WriteAllText(localPath, "[\n");
-                }
-                else
-                {
-                    string content = File.ReadAllText(localPath).TrimEnd();
+					// Si no existe el archivo, lo creamos y escribimos el inicio del JSON
+					if (!File.Exists(localPath))
+					{
+						// No existe: lo creamos y escribimos [
+						File.WriteAllText(localPath, "[\n");
+					}
+					else
+					{
+						string content = File.ReadAllText(localPath).TrimEnd();
 
-                    // Existe pero está vacío
-                    if (string.IsNullOrWhiteSpace(content))
-                    {
-                        File.WriteAllText(localPath, "[\n");
-                    }
-                    else
-                    {
+						// Existe pero está vacío
+						if (string.IsNullOrWhiteSpace(content))
+						{
+							File.WriteAllText(localPath, "[\n");
+						}
+						else
+						{
 
-                        // Quitamos el cierre, la coma se escribirá luego, pero vamos a introducir un salto de línea para diferenciar entre sesiones.
-                        int lastBracketIndex = content.LastIndexOf(']');
-                        if (lastBracketIndex != -1)
-                        {
-                            content = content.Substring(0, lastBracketIndex).TrimEnd();
-                            File.WriteAllText(localPath, content + "\n");
-                        }
-                    }
-                }
-                break;
-            default: break;
-        }
-        Debug.Log("Path to telemetry log file: " + localPath);
+							// Quitamos el cierre, la coma se escribirá luego, pero vamos a introducir un salto de línea para diferenciar entre sesiones.
+							int lastBracketIndex = content.LastIndexOf(']');
+							if (lastBracketIndex != -1)
+							{
+								content = content.Substring(0, lastBracketIndex).TrimEnd();
+								File.WriteAllText(localPath, content + "\n");
+							}
+						}
+					}
+					break;
+				default: break;
+			}
+			Debug.Log("Path to telemetry log file: " + localPath);
+		}
+        catch (Exception ex)
+        {
+			Debug.LogError($"[FilePersistence] There was an error while opening the file: {ex.Message}");
+		}
+      
     }
 
     /// <summary>
@@ -74,53 +76,69 @@ public class FilePersistence : Persistence
     /// </summary>
     public override void FlushQueue(CircularQueue<Event> eventQueue)
     {
-        int i = 0;
-        bool isFirst = IsFirstJsonEntry();
 
-        StringBuilder batch = new StringBuilder();
-
-        while (eventQueue.TryDequeue(out Event e))
+        try
         {
-            if (e != null)
-            {
-                string data;
-                switch (format)
-                {
-                    case Format.JSON:
-                        data = e.ToJSON();
-                        break;
-                    case Format.CSV:
-                        data = e.ToCSV();
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(format), format, null);
-                }
+			int i = 0;
+			bool isFirst = IsFirstJsonEntry();
 
-                if (format == Format.JSON)
-                {
-                    // En caso de no ser el primero, necesita una coma delante.
-                    if (!isFirst)
-                        batch.Append(",\n");
-                    batch.Append(data);
-                    isFirst = false;
-                }
-                else
-                {
-                    batch.AppendLine(data);
-                }
-            }
-            i++;
-        }
+			StringBuilder batch = new StringBuilder();
 
-        if (batch.Length > 0)
-            File.AppendAllText(localPath, batch.ToString());
+			while (eventQueue.TryDequeue(out Event e))
+			{
+				if (e != null)
+				{
+					string data;
+					switch (format)
+					{
+						case Format.JSON:
+							data = e.ToJSON();
+							break;
+						case Format.CSV:
+							data = e.ToCSV();
+							break;
+						default:
+							throw new ArgumentOutOfRangeException(nameof(format), format, null);
+					}
+
+					if (format == Format.JSON)
+					{
+						// En caso de no ser el primero, necesita una coma delante.
+						if (!isFirst)
+							batch.Append(",\n");
+						batch.Append(data);
+						isFirst = false;
+					}
+					else
+					{
+						batch.AppendLine(data);
+					}
+				}
+				i++;
+			}
+
+			if (batch.Length > 0)
+				File.AppendAllText(localPath, batch.ToString());
+		}
+		catch (Exception ex)
+		{
+			Debug.LogError($"[FilePersistence] Error while writing the events in the file: {ex.Message}\n");
+		}
+        
     }
     private bool IsFirstJsonEntry()
     {
-        if (!File.Exists(localPath)) return true;
+		try
+		{
+			if (!File.Exists(localPath)) return true;
 
-        string content = File.ReadAllText(localPath).Trim();
+			string content = File.ReadAllText(localPath).Trim();
 
-        return content == "[" || content == "[\n";
+			return content == "[" || content == "[\n";
+		}
+		catch(Exception ex) 
+		{
+			throw new InvalidOperationException("Critical error: Unable to read JSON log file.", ex);
+		}
     }
 }
