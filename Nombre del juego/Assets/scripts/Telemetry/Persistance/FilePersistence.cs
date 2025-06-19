a32d7f040dc29cbc8d10c5ef3a1534e00b1ab1b9
Andrés García Navarro <andrga37@ucm.es>
2025-06-19 14:33:15 +0200

    renombrado de objetos

--- Nombre del juego/Assets/scripts/Telemetry/Persistance/FilePersistence.cs
+++ Nombre del juego/Assets/scripts/Telemetry/Persistance/FilePersistence.cs
@@ -7,14 +7,13 @@ public class FilePersistence : Persistence
 {
     private string localPath;   // Ruta en donde se guarda el archivo con los datos
     private ISerializer serializer; // Formato de los eventos
-	
+
     public FilePersistence(ISerializer _format) : base()
     {
-		serializer = _format;
+        serializer = _format;
 
         CreateLocalLogFile();
     }
-
 
     /// <summary>
     /// Saca de la cola cuando se superen cierto elementos y escribe en el archivo en el formato
@@ -26,32 +25,32 @@ public class FilePersistence : Persistence
 
         try
         {
-			int i = 0;
-			bool isFirst = IsFirstJsonEntry();
+            int i = 0;
+            bool isFirst = IsFirstJsonEntry();
 
-			StringBuilder batch = new StringBuilder();
+            StringBuilder batch = new StringBuilder();
 
-			while (eventQueue.TryDequeue(out Event e))
-			{
-				if (e != null)
-				{
+            while (eventQueue.TryDequeue(out Event e))
+            {
+                if (e != null)
+                {
                     string data;
 
                     data = serializer.Serialize(e); //Serializamos el evento
                     serializer.AppendSerializedData(batch, data, ref isFirst);  //Escribimos el evento siguiendo el formato
 
-				}
-				i++;
-			}
+                }
+                i++;
+            }
 
-			if (batch.Length > 0)
-				File.AppendAllText(localPath, batch.ToString());
-		}
-		catch (Exception ex)
-		{
-			Debug.LogError($"[FilePersistence] Error while writing the events in the file: {ex.Message}\n");
-		}
-        
+            if (batch.Length > 0)
+                File.AppendAllText(localPath, batch.ToString());
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"[FilePersistence] Error while writing the events in the file: {ex.Message}\n");
+        }
+
     }
 
     /// <summary>
@@ -65,18 +64,18 @@ public class FilePersistence : Persistence
 
     private bool IsFirstJsonEntry()
     {
-		try
-		{
-			if (!File.Exists(localPath)) return true;
+        try
+        {
+            if (!File.Exists(localPath)) return true;
 
-			string content = File.ReadAllText(localPath).Trim();
+            string content = File.ReadAllText(localPath).Trim();
 
-			return content == "[" || content == "[\n";
-		}
-		catch(Exception ex) 
-		{
-			throw new InvalidOperationException("Critical error: Unable to read JSON log file.", ex);
-		}
+            return content == "[" || content == "[\n";
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidOperationException("Critical error: Unable to read JSON log file.", ex);
+        }
     }
 
 
@@ -108,7 +107,7 @@ public class FilePersistence : Persistence
                     File.WriteAllText(localPath, serializer.initFile());
                 }
                 //Existe y tiene texto
-                else 
+                else
                 {
                     int lastBracketIndex = 0;
                     if (serializer.changeOfSesion(ref content, lastBracketIndex))
--- Nombre del juego/Assets/scripts/Telemetry/Persistence.cs
+++ Nombre del juego/Assets/scripts/Telemetry/Persistence.cs
@@ -1,14 +1,3 @@
-using Firebase;
-using Firebase.Database;
-using Firebase.Extensions;
-using System;
-using System.Collections.Concurrent;
-using System.IO;
-using System.Net.Http;
-using System.Net.Mail;
-using System.Text;
-using UnityEngine;
-using static Tracker;
 
 public class Persistence
 {
